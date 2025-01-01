ad5b6828e3331e0f701abf8eff3b220ecf6c9e7e
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-01 15:41:47 +0900

    JavaScript - display items 학습완료

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -7,12 +7,33 @@ function loadItems() {
         // json으로 변환 시, 배열 object "items"를 가져옴
 }
 
+// 로드한 items를 화면에 보여줄 수 있도록 html 태그화
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    /*
+    map().join('')의 원리 파악하기!
+    const HTML = items.map(item => createHTMLString(item)).join('');
+    console.log(HTML);
+    */
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+    // innerHTML의 이름을 item으로 지정해줄 수 있음
+}
+
+// 불러온 데이터의 key들을 html 태그로 직접 변환하는 함수
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />            
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 // MAIN
 loadItems()
     .then(items => {
         console.log(items);
-        //displayItems(items);
-        /* 로드한 items를 화면에 보여줄 수 있도록 */
+        displayItems(items);
         //setEventListeners(items);
     })
     .catch(console.log);
