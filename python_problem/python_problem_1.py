c4a52ee561eea24b6f62103f241b4f4f34d41d6f
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-03 14:31:03 +0900

    python_problem_1 5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -7,7 +7,8 @@ class noneIntNum(Exception):
 class outOfRange(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
-        
+
+### turn: playerA       
 try:
     aNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
     if (aNum!=1 and aNum!=2 and aNum!=3):
@@ -24,4 +25,19 @@ except outOfRange as e1:
 except noneIntNum as e2:
         print(e2)
 
-aNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+### turn: playerB    
+try:
+    bNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+    if (bNum!=1 and bNum!=2 and bNum!=3):
+        raise outOfRange
+    elif (isinstance(bNum, int)):
+        for i in range(1, bNum+1):
+            print('playerB : {0}'.format(num+i))
+        num = num+bNum
+    else:
+        # bNum이 정수가 아니라면
+        raise noneIntNum    
+except outOfRange as e1:
+        print(e1)
+except noneIntNum as e2:
+        print(e2)
