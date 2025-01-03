28920e375c41f3194b0c215c64b435c9cd26e8bd
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-03 16:17:48 +0900

    python_problem_1 6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,5 +1,4 @@
 num = 0
-
 class noneIntNum(Exception):
     def __init__(self):
         super().__init__('정수를 입력하세요')
@@ -8,36 +7,43 @@ class outOfRange(Exception):
     def __init__(self):
         super().__init__('1,2,3 중 하나를 입력하세요')
 
-### turn: playerA       
-try:
-    aNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-    if (aNum!=1 and aNum!=2 and aNum!=3):
-        raise outOfRange
-    elif (isinstance(aNum, int)):
-        for i in range(1, aNum+1):
-            print('playerA : {0}'.format(num+i))
-        num = num+aNum
-    else:
-        # aNum이 정수가 아니라면
-        raise noneIntNum    
-except outOfRange as e1:
-        print(e1)
-except noneIntNum as e2:
-        print(e2)
+gameTurn = True
+# True = playerA, False = playerB
 
-### turn: playerB    
-try:
-    bNum = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-    if (bNum!=1 and bNum!=2 and bNum!=3):
-        raise outOfRange
-    elif (isinstance(bNum, int)):
-        for i in range(1, bNum+1):
-            print('playerB : {0}'.format(num+i))
-        num = num+bNum
-    else:
-        # bNum이 정수가 아니라면
-        raise noneIntNum    
-except outOfRange as e1:
-        print(e1)
-except noneIntNum as e2:
-        print(e2)
+def brGame(num, gameTurn):
+    try:
+        answer = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        try:
+            aNum = int(answer)
+        except ValueError:
+            raise noneIntNum
+        
+        if (aNum!=1 and aNum!=2 and aNum!=3):
+            # aNum이 1,2,3의 숫자가 아니라면
+            raise outOfRange
+        elif (not isinstance(aNum, int)):
+            # aNum이 정수가 아니라면
+            raise noneIntNum
+        else:
+            for i in range(1, aNum+1):
+                if(gameTurn):
+                    print('playerA : {0}'.format(num+i))
+                    if(num+i == 31):
+                        return
+                else:
+                    print('playerB : {0}'.format(num+i))
+                    if(num+i == 31):
+                        return
+            num = num+aNum
+            gameTurn = not gameTurn # swith player turn
+            return num, gameTurn
+    except outOfRange as e1:
+            print(e1)
+    except noneIntNum as e2:
+            print(e2)
+    return num, gameTurn
+                        
+while(num < 31):
+    num, gameTurn = brGame(num, gameTurn)
+    if(num==-1):
+        break
