0f1485eee9c7612e1c9489a68ce53a45ad0c13af
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-08 14:19:22 +0900

    djangogirls - 애플리케이션 확장하기 학습완료

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name="post_list"),
     # 127.0.0.1:8000/ 접속 시 views.post_list를 보여줌
+    path('post/<int:pk>/', views.post_detail, name="post_detail"),
 ]
