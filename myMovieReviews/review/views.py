492dfaf12a27d7174c93a112f52189e02c47d070
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-10 22:01:55 +0900

    add: 백엔드 챌린지 구현 추가

--- myMovieReviews/review/views.py
+++ myMovieReviews/review/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from .models import Review
+from .forms import GenreForm
 
 def review_list(request):
     reviews = Review.objects.all()
@@ -9,8 +10,15 @@ def review_list(request):
     }
     return render(request, 'review/list.html', context)
 
+def calc_running_time(running_time):
+    total_time = int(running_time.replace('분', '').strip())
+    hour = total_time // 60
+    min = total_time % 60
+    return f"{hour}시간 {min}분"
+    
 def review_detail(request, pk):
     review = Review.objects.get(id=pk)
+    review.running_time = calc_running_time(review.running_time)
     context = {
         'review': review
     }
@@ -18,11 +26,14 @@ def review_detail(request, pk):
 
 def review_create(request):
     if request.method == 'POST':
+        genre_form = GenreForm(request.POST)
+        if genre_form.is_valid():
+            genre = genre_form.cleaned_data['genre']
         review = Review.objects.create(
             poster = request.FILES['poster'],
             title = request.POST['title'],
             release = request.POST['release'],
-            genre = request.POST['genre'],
+            genre = genre,
             stars = request.POST['stars'],
             director = request.POST['director'],
             actor = request.POST['actor'],
@@ -30,7 +41,8 @@ def review_create(request):
             summary = request.POST['summary'],
         )
         return redirect(reverse('review:review_list'))
-    return render(request, 'review/create.html')
+    genre_for_get = GenreForm()
+    return render(request, 'review/create.html', {'genre_form': genre_for_get})
 
 def review_update(request, pk):
     exist_review = Review.objects.get(id=pk)
@@ -38,16 +50,19 @@ def review_update(request, pk):
         poster = request.FILES.get('poster') or exist_review.poster
         title = request.POST.get('title')
         release = request.POST.get('release')
-        genre = request.POST.get('genre')
+        genre = GenreForm(request.POST, instance=exist_review)
         stars = request.POST.get('stars')
         director = request.POST.get('director')
         actor = request.POST.get('actor')
         running_time = request.POST.get('running_time')
         summary = request.POST.get('summary')
+        
+        if genre.is_valid():
+            store_genre = genre.cleaned_data['genre']
         exist_review.poster = poster
         exist_review.title = title
         exist_review.release = release
-        exist_review.genre = genre
+        exist_review.genre = store_genre
         exist_review.stars = stars
         exist_review.director = director
         exist_review.actor = actor
@@ -56,8 +71,10 @@ def review_update(request, pk):
         exist_review.save()
         return redirect('review:review_detail', pk=exist_review.pk)
     
+    genre_for_get = GenreForm(instance=exist_review)
     context = {
-        'review': exist_review
+        'review': exist_review,
+        'genre_form': genre_for_get,
     }
     return render(request, 'review/update.html', context)
 
