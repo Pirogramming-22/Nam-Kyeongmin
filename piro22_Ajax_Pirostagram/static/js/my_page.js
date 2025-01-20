f1db4a967701329f91f0e98f5551f54e01e51062
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-01-20 18:06:31 +0900

    [add]: 댓글 삭제 기능 구현추가

--- piro22_Ajax_Pirostagram/static/js/my_page.js
+++ piro22_Ajax_Pirostagram/static/js/my_page.js
@@ -17,16 +17,29 @@ function onClickComment() {
     const inputField = document.querySelector('.comment_input');
     const inputValue = inputField.value;
     const commentParent = document.querySelector('.uploaded_comment_container');
+    const trash_icon_url = commentParent.getAttribute('data-trash-icon');
+
     const newComment = document.createElement('div');
     newComment.classList.add('upload_comment');
 
     newComment.innerHTML = `
         <div class="commet_author">episode.nkm</div>
         <div class="comment_content">${inputValue}</div>
+        <button type="submit" class="delete_comment_btn" onclick="onClickDelete(this)">
+            <img src="${trash_icon_url}" alt="댓글 삭제 버튼" />
+        </button>
     `;
 
     commentParent.appendChild(newComment);
     inputField.value = '';
+}
+
+function onClickDelete(this_comment) {
+    const want_delete = this_comment.closest('.upload_comment');
+    const alert = confirm("해당 댓글을 삭제하시겠습니까?");
+    if (alert) {
+        want_delete.remove();
+    }
 }
 
 requestLike.onreadystatechange = () => {
